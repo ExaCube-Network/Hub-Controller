bfa06a6a2ee66fae78271e0ff7b52e2841991671
xhayper <ikq31421@gmail.com>
2020-06-21 12:25:51 +0700

    Remove unnecessary cast, change coding order.

--- src/main/java/be/exacu/Spigot/Listeners/BlockModifyCancel.java
+++ src/main/java/be/exacu/Spigot/Listeners/BlockModifyCancel.java
@@ -36,7 +36,7 @@ public class BlockModifyCancel implements Listener {
         Material[] trapDoors = {Material.ACACIA_TRAPDOOR, Material.BIRCH_TRAPDOOR, Material.DARK_OAK_TRAPDOOR, Material.IRON_TRAPDOOR, Material.JUNGLE_TRAPDOOR, Material.OAK_TRAPDOOR, Material.SPRUCE_TRAPDOOR};
         Player p = e.getPlayer();
         if (e.getClickedBlock() != null) {
-            if (Arrays.stream(trapDoors).anyMatch(e.getClickedBlock().getType()::equals)) {
+            if (Arrays.asList(trapDoors).contains(e.getClickedBlock().getType())) {
                 if (!p.hasPermission("exacube.hub.modifyblocks")) {
                     e.setCancelled(true);
                 }
@@ -50,7 +50,7 @@ public class BlockModifyCancel implements Listener {
         Material[] doors = {Material.DARK_OAK_DOOR, Material.ACACIA_DOOR, Material.IRON_DOOR, Material.BIRCH_DOOR, Material.JUNGLE_DOOR, Material.OAK_DOOR, Material.SPRUCE_DOOR};
         Player p = e.getPlayer();
         if (e.getClickedBlock() != null) {
-            if (Arrays.stream(doors).anyMatch(e.getClickedBlock().getType()::equals)) {
+            if (Arrays.asList(doors).contains(e.getClickedBlock().getType())) {
                 if (!p.hasPermission("exacube.hub.modifyblocks")) {
                     e.setCancelled(true);
                 }
@@ -64,7 +64,7 @@ public class BlockModifyCancel implements Listener {
         Material[] fenceGates = {Material.ACACIA_FENCE_GATE, Material.BIRCH_FENCE_GATE, Material.DARK_OAK_FENCE_GATE, Material.JUNGLE_FENCE_GATE, Material.OAK_FENCE_GATE, Material.SPRUCE_FENCE_GATE};
         Player p = e.getPlayer();
         if (e.getClickedBlock() != null) {
-            if (Arrays.stream(fenceGates).anyMatch(e.getClickedBlock().getType()::equals)) {
+            if (Arrays.asList(fenceGates).contains(e.getClickedBlock().getType())) {
                 if (!p.hasPermission("exacube.hub.modifyblocks")) {
                     e.setCancelled(true);
                 }
